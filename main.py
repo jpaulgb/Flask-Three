8feef6d3d8eeccf07a71babd3f8642e76bd751ed
jpaulgb <jepgb@hotmail.com>
2025-07-01 05:56:12 -0500

    porque pides 8

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ app = Flask(__name__)
 #creamos el servidor y lo guardamos en app
 #rutas:
 #defino una ruta /app/v1/users e asociamos ruta
-@app.route('/app/v1/users')
-def user_action():
-    print("estoy aca")
-    return "tu usuario"
+@app.route('/app/v1/users/<id>')
+def user_action(id):
+    if(id == "8"):
+        return "porque me pedis 8"
+    else:
+        return id
 
 #siempre se pone al final:
 app.run(debug=True)
