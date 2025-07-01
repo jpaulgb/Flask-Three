36516f370f57abec4159d95afe473343c84071fc
jpaulgb <jepgb@hotmail.com>
2025-07-01 05:45:37 -0500

    retur tu usuario

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from flask import Flask
 
 app = Flask(__name__)
+#creamos el servidor y lo guardamos en app
+#rutas:
+#defino una ruta /app/v1/users e asociamos ruta
+@app.route('/app/v1/users')
+def user_action():
+    print("estoy aca")
+    return "tu usuario"
 
-print(__name__)
+#siempre se pone al final:
+app.run(debug=True)
